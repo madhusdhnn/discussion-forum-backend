ca2b78c550e6c8078f368009b6438d5e36912373
Madhusudhanan <madhusdhnn.ms@gmail.com>
2022-12-28 17:36:12 +0530

    Initial commit

---
+++ test/discussion_forum_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DiscussionForumBackend from '../lib/discussion_forum_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/discussion_forum_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DiscussionForumBackend.DiscussionForumBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
